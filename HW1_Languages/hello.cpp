1b4b9ce0df8ee21811f1d778d9efdf0108a873f7
Taylor Hancock <58285541+SteelStarling@users.noreply.github.com>
2023-04-04 15:33:57 -0700

    Added HW1 to repo from school servers via MobaXTerm

---
+++ HW1_Languages/hello.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+// namespace scoping to support large distributed code
+using namespace std;
+
+void hello(string name);
+
+int main(int argc, char* argv[]) {
+    out << "Hello C++!" << endl; 
+    // argc is number of command line tokens 
+    for (int i = 0; i < argc; ++i) 
+        // string is a constructor and returns a string object 
+        hello(string(argv[i])); 
+    return 0; 
+} 
+ 
+void hello(string name) { 
+    cout << "Hello " << name << endl; 
+} 
---
+++ HW1_Languages/hello.pl
@@ -0,0 +1,7 @@
+use strict;
+use warnings;
+
+print("Hello Perl!\n");
+while (<>) {
+    print("Hello " . join("\nHello ", split(/\s+/)) . "\n");
+}
